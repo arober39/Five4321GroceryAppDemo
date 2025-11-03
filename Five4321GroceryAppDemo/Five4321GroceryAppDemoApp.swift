59c49f16d8695f36cc1ad662c6e8db0c687ea0f2
Alexis Roberson <aroberson@launchdarkly.com>
2025-11-03 14:23:48 -0600

    adding grocery list app

---
+++ Five4321GroceryAppDemo/Five4321GroceryAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Five4321GroceryAppDemoApp.swift
+//  Five4321GroceryAppDemo
+//
+//  Created by Alexis Roberson on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Five4321GroceryAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Five4321GroceryAppDemoTests/Five4321GroceryAppDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  Five4321GroceryAppDemoTests.swift
+//  Five4321GroceryAppDemoTests
+//
+//  Created by Alexis Roberson on 11/3/25.
+//
+
+import Testing
+
+struct Five4321GroceryAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
